f4c9f92d724d1132c3f00cfe66549e15e43bbdc1
王贺辉 <wanghehui@bjknrt.com>
2022-10-11 17:00:56 +0800

    时间

--- kotlin-data-server/src/test/kotlin/com/bjknrt/kotlin/data/api/HealthPlanTest.kt
+++ kotlin-data-server/src/test/kotlin/com/bjknrt/kotlin/data/api/HealthPlanTest.kt
@@ -23,72 +23,7 @@ class HealthPlanTest : AbstractContainerBaseTest() {
     @Test
     fun clockInTest() {
         val body: java.math.BigInteger = TODO()
-        val response: List<HealthPlan> = api.clockIn(body)
-
-        // TODO: test validations
-    }
-
-    /**
-     * To test HealthPlanApi.frequencyGetClockIn
-     */
-    @Test
-    fun frequencyGetClockInTest() {
-        val frequencyGetClockInParam: FrequencyGetClockInParam = TODO()
-        val response: kotlin.Int = api.frequencyGetClockIn(frequencyGetClockInParam)
-
-        // TODO: test validations
-    }
-
-    /**
-     * To test HealthPlanApi.healthPlanFrequencyGetClockIn
-     */
-    @Test
-    fun healthPlanFrequencyGetClockInTest() {
-        val healthPlanFrequencyGetClockInParam: HealthPlanFrequencyGetClockInParam = TODO()
-        val response: kotlin.Int = api.healthPlanFrequencyGetClockIn(healthPlanFrequencyGetClockInParam)
-
-        // TODO: test validations
-    }
-
-    /**
-     * To test HealthPlanApi.idGetList
-     */
-    @Test
-    fun idGetListTest() {
-        val id: kotlin.collections.List<Id> = TODO()
-        val response: List<HealthPlanMain> = api.idGetList(id)
-
-        // TODO: test validations
-    }
-
-    /**
-     * To test HealthPlanApi.list
-     */
-    @Test
-    fun listTest() {
-        val response: List<HealthPlan> = api.list()
-
-        // TODO: test validations
-    }
-
-    /**
-     * To test HealthPlanApi.patientIdGetList
-     */
-    @Test
-    fun patientIdGetListTest() {
-        val patientIdGetListParam: PatientIdGetListParam = TODO()
-        val response: List<HealthPlanMain> = api.patientIdGetList(patientIdGetListParam)
-
-        // TODO: test validations
-    }
-
-    /**
-     * To test HealthPlanApi.upsertTypeFrequencyHealth
-     */
-    @Test
-    fun upsertTypeFrequencyHealthTest() {
-        val frequencyHealthAllParam: FrequencyHealthAllParam = TODO()
-        val response: List<UpsertHealthFrequencyResult> = api.upsertTypeFrequencyHealth(frequencyHealthAllParam)
+        val response = api.clockIn(body)
 
         // TODO: test validations
     }
